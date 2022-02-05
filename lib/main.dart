1b8d7c250afef95c89cb5bc60e35b043e0bd6c00
Rahul Khatri <khatri.rahul019@gmail.com>
2022-02-05 14:17:56 +0530

    Image load on Local Device, App Bar with Text and Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  imageLocal();
+ // imageLocal();
+ //  triImage();
+  appBarWidget();
+}
+
+void appBarWidget(){
+  runApp(
+    MaterialApp(
+      title: "My App",
+    home: Scaffold(
+    appBar: AppBar(
+      title: const Text("My Flutter App", textDirection: TextDirection.ltr,),
+      backgroundColor: Colors.amberAccent,
+    ),
+      body: const Align(child: Text("Hello, Flutter", textDirection: TextDirection.ltr,),),
+    )
+    )
+  );
 }
 
 void imageLocal(){
 runApp(Column(
   children: <Widget>[
     Image.asset("assets/images/avatar.jpg"),
-    AssetImage('assets/images/avatar.jpg')
+    Image(image:AssetImage('assets/images/avatar.jpg'))
   ],
 ));
 }
 
+void triImage(){
+  runApp(Column(
+    children: [
+      Container(height: 200, child: Image.network("https://picsum.photos/200"),),
+      Container(height: 200, child: Image.asset("assets/images/avatar.jpg"),),
+      Container(height:200,child:Image.file(File("storage/emulated/0/Pictures/IMG_20220205_114138.jpg"))),
+    ],
+  ));
+}
