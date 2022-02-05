e871470fd8ad7db3ed0f82e13071afc0fd7dd336
Rahul Khatri <khatri.rahul019@gmail.com>
2022-02-05 17:23:17 +0530

    Drawer by stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,69 @@ void main() {
  //  triImage();
   appBarWidget();
 }
-
+class DrawerWidget extends StatelessWidget{
+  var itemMap = {
+    {'icon': Icons.person, 'title':"My Profile"},
+    {'icon': Icons.table_view, 'title': "Statistics"},
+    {'icon': Icons.settings, 'title': "Settings"}
+  };
+   @override
+  Widget build(BuildContext context){
+     return Drawer(
+       child: ListView(
+         children: [
+           DrawerHeader(child: Container(color: Colors.blue,)),
+           for(var item in itemMap)
+             ListTile(leading: Icon(item['icon'] as IconData), title: Text(item['title'] as String),)
+         ],
+       ),
+     );
+   }
+}
 void appBarWidget(){
   runApp(
     MaterialApp(
       title: "My App",
     home: Scaffold(
     appBar: AppBar(
-      title: const Text("My Flutter App", textDirection: TextDirection.ltr,),
+      leading: Icon(Icons.menu),
+      actions: [
+        Icon(Icons.email),
+        Icon(Icons.more_vert)
+      ],
+      title: const Text("My Flutter App", textDirection: TextDirection.ltr),
       backgroundColor: Colors.amberAccent,
     ),
-      body: const Align(child: Text("Hello, Flutter", textDirection: TextDirection.ltr,),),
+      drawer: DrawerWidget(),
+      // drawer: Drawer(
+      //   child: Column(
+      //     children: [
+      //       Container(height: 40,),
+      //       Row(children: [
+      //         Icon(Icons.person),
+      //         Text("Profile")
+      //       ],),
+      //       Row(children: [
+      //         Icon(Icons.table_chart),
+      //         Text("Statistics"),
+      //       ],),
+      //       Row(
+      //         children: [
+      //           Icon(Icons.settings),
+      //           Text("Settings")
+      //         ],
+      //       )
+      //     ],
+      //   ),
+      // ),
+      body: const Align(child: Text("Hello, Flutter App"),),
     )
     )
   );
 }
-
+//  Profile face icon
+//  Statistic table icon
+//   Settings gear icon
 void imageLocal(){
 runApp(Column(
   children: <Widget>[
